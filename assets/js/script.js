b98bcb236c2ee0a26a53fbd55afc185756c95b69
Bhavna Jain <bhavnaostwal@gmail.com>
2022-04-07 22:41:57 -0400

    added basic structure and styling to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+//open weather API key
 var APIKey = "7fff4023f1901b4e3539220a32e09afa";
+
+//function to search current weather of searched city
+function searchCity(event){
+
+    event.preventDefault();
+    var cityInput = $("#searchcity").val();
+
+    if(cityInput === ""){
+        alert("Please select a city");
+    }
+
+    searchCurrentWeather(cityInput);
+    displaySearchHistory(cityInput);
+
+    $("#searchcity").val("");
+
+}
